ed0e25d4d983c736e8d8b4b9af383a9e29d702f2
Ryan Tsao <rtsao@uber.com>
2016-04-08 16:37:41 -0700

    Add important to generated css

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var passthrough = require('../passthrough');
 
 test('basic usage', function(t) {
   var result = epistyle({color: 'red', ':hover': {color: 'blue'}});
-  t.equal(result.css, '._style_3cbjp0 {\n  color: red\n}\n._style_3cbjp0:hover {\n  color: blue\n}');
+  t.equal(result.css,'._style_3cbjp0 {\n  color: red !important\n}\n._style_3cbjp0:hover {\n  color: blue !important\n}');
   t.equal(result.className, '_style_3cbjp0');
   t.end();
 });
@@ -14,7 +14,7 @@ test('basic usage', function(t) {
 test('basic passthrough usage', function(t) {
   var result = passthrough({color: 'red', ':hover': {color: 'blue'}});
   t.deepEqual(result.styles, {color: 'red'});
-  t.equal(result.css, '._style_1KAmyC:hover {\n  color: blue\n}');
+  t.equal(result.css, '._style_1KAmyC:hover {\n  color: blue !important\n}');
   t.equal(result.className, '_style_1KAmyC');
   t.end();
 });
